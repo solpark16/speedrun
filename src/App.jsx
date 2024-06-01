2a1ed0c097366e272a196f1bec278fb2e5456a1f
Minsoo Cho <textign@gmail.com>
2024-06-01 15:35:12 +0900

    fix: 리액트 쿼리 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/router";
 import GlobalStyle from "./style/GrobalStyle";
 
 function App() {
-	const queryClient = new QueryClient();
-
 	return (
 		<>
-			<QueryClientProvider client={queryClient}>
-				<GlobalStyle />
-				<RouterProvider router={router}></RouterProvider>
-			</QueryClientProvider>
+			<GlobalStyle />
+			<RouterProvider router={router}></RouterProvider>
 		</>
 	);
 }
