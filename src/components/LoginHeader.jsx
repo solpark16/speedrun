9fdc475e51fa1a0e4d3f3788ff67ee796f45e49e
요니'^ <jjy606@naver.com>
2024-06-03 19:14:40 +0900

    update:로그인 수정

--- src/components/LoginHeader.jsx
+++ src/components/LoginHeader.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const HeaderBox = styled.div`
+const StyledHeader = styled.header`
+	padding: 20px 0;
+	box-shadow: 0px 3px 3px 0px rgba(0, 0, 0, 0.25);
+`;
+const StyledHeaderBox = styled.div`
 	display: flex;
-	align-items: center;
 	justify-content: center;
+	align-items: center;
+`;
+const StyleLogoBox = styled.h1`
+	width: 162px;
+	img {
+		width: 100%;
+	}
+`;
+
+const Logo = styled.img`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const LoginBox = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-top: 5rem;
 `;
 
 const Title = styled.h2`
@@ -12,17 +36,28 @@ const Title = styled.h2`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	font-size: 2.5rem;
+	margin-top: 3.5rem;
 `;
-
-const Logo = styled.img``;
 
 function LoginHeader() {
 	return (
 		<>
-			<HeaderBox>
-				<Logo src="imgs/speed-run-login.png" alt="로그인 로고" />
-				<Title>Login</Title>
-			</HeaderBox>
+			<StyledHeader>
+				<div className="container">
+					<StyledHeaderBox>
+						<StyleLogoBox>
+							<Link to="/">
+								<img src="/imgs/speed-run-logo.png" alt="스피드런 로고" />
+							</Link>
+						</StyleLogoBox>
+					</StyledHeaderBox>
+				</div>
+			</StyledHeader>
+			<LoginBox>
+				<Logo src="imgs/speed-run-login.png" alt="로그인" />
+			</LoginBox>
+			<Title>LOGIN</Title>
 		</>
 	);
 }
--- src/components/LoginInput.jsx
+++ src/components/LoginInput.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 
-function LoginInput() {
-	return;
+const Input = styled.input`
+	width: 250px;
+	padding: 10px;
+	border: 1px solid #ccc;
+	margin: 10px 0;
+	margin-top: 3rem;
+`;
+
+function LoginInput({ type, placeholder, value, onChange }) {
+	return (
+		<>
+			<Input type={type} placeholder={placeholder} value={value} onChange={onChange} />
+		</>
+	);
 }
 
 export default LoginInput;
