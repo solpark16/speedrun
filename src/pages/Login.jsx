22f87ceebdd5a633d4e975dc545eb30e1766bda8
요니'^ <jjy606@naver.com>
2024-06-03 11:04:18 +0900

    add: login 브랜치 추가

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,17 @@
+import LoginButton from "../components/LoginButton";
+import LoginHeader from "../components/LoginHeader";
+import LoginInput from "../components/LoginInput";
+import LoginLinks from "../components/LoginLinks";
+
 function Login() {
-  return <div>Login</div>;
+	return (
+		<>
+			<LoginHeader />
+			<LoginInput />
+			<LoginButton />
+			<LoginLinks />
+		</>
+	);
 }
 
 export default Login;
