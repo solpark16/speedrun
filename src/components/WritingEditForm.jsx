f232e6cd9b49c22738ba4dab3b015b6f5b95c237
Minsoo Cho <textign@gmail.com>
2024-06-04 11:30:06 +0900

    fix : 하트 수정 + 글쓰기 페이지 리덕스 삭제

--- src/components/WritingEditForm.jsx
+++ src/components/WritingEditForm.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import styled from "styled-components";
 import supabase from "../supabase/supabase";
-import { useNavigate, useParams } from "react-router-dom";
 
 const StyledTitleInput = styled.input`
 	font-family: inherit;
--- src/utils/getNewsfeed.js
+++ src/utils/getNewsfeed.js
@@ -1,13 +1,18 @@
 import supabase from "../supabase/supabase";
 
 export async function getNewsfeed() {
-	const { data: newsfeed, error } = await supabase.from("newsfeed").select("*");
+	const { data: newsfeed, error } = await supabase.from("newsfeed").select("*").order("date", { ascending: false });
 	if (error) throw error;
 	return newsfeed;
 }
 
+export async function getNewsfeedLike(id) {
+	const { data: like, error } = await supabase.from("newsfeed").select("like").eq("id", id);
+	if (error) throw error;
+	return like;
+}
+
 export async function updateNewsfeedLike({ like, id }) {
-	console.log(id);
 	const { error } = await supabase.from("newsfeed").update({ like: like }).eq("id", id);
 	if (error) throw error;
 }
