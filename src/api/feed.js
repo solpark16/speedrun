5eb9ae45cfac5cc50ea927a7d7deeb20d8ca6fa6
Minsoo Cho <textign@gmail.com>
2024-06-05 14:29:16 +0900

    찐으로 좋아요 해결함

--- src/api/feed.js
+++ src/api/feed.js
@@ -34,7 +34,7 @@ export async function addNewsfeedLike({ userId, feedId }) {
 	if (error) throw error;
 }
 
-export async function removNewsfeedLike({ feedId }) {
-	const { error } = await supabase.from("like").delete().eq("feedId", feedId);
+export async function removNewsfeedLike({ userId, feedId }) {
+	const { error } = await supabase.from("like").delete().eq("feedId", feedId).eq("userId", userId);
 	if (error) throw error;
 }
--- src/components/join_login/LoginInput.jsx
+++ src/components/join_login/LoginInput.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { getLoginUser } from "../../api/auth";
 import { logInToggle, setCurrentUser } from "../../redux/slices/user.slice";
+import LoginButton from "./LoginButton";
 
 function LoginInput() {
 	const dispatch = useDispatch();
@@ -106,55 +107,5 @@ const StyledPasswordInput = styled.input`
 	background-position: 27px center;
 	box-sizing: border-box;
 `;
-
-function LoginInput() {
-	const dispatch = useDispatch();
-	const navigate = useNavigate();
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const [user, setUser] = useState(null);
-
-	const handleLogin = async (e) => {
-		e.preventDefault();
-		const loginInfo = {
-			email,
-			password
-		};
-		const userData = await getLoginUser(loginInfo);
-		setUser(userData.user);
-
-		if (userData) {
-			dispatch(logInToggle(true));
-			dispatch(setCurrentUser(userData.user));
-			navigate(-1);
-		} else {
-			alert("로그인에 실패했습니다.");
-		}
-	};
-
-	return (
-		<>
-			{!user ? (
-				<StyledFormBox onSubmit={handleLogin}>
-					<StyledInputForm
-						type="email"
-						placeholder="이메일을 입력해주세요"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-					<StyledPasswordInput
-						type="password"
-						placeholder="비밀번호를 입력해주세요"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-					<LoginButton onClick={handleLogin} />
-				</StyledFormBox>
-			) : (
-				<div>이미 로그인 되었습니다.</div>
-			)}
-		</>
-	);
-}
 
 export default LoginInput;
